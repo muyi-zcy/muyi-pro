59b88bf4b97a09472076360c06c51eebdbeebd54
muyi <zcy_nemo@aliyun.com>
2020-12-13 20:32:07 +0800

    leetcode_算法_217_存在重复元素

---
+++ src/main/java/com/muyi/leetcode/algorithms/leetcode_217_contains_duplicate.java
@@ -0,0 +1,47 @@
+package com.muyi.leetcode.algorithms;
+
+import java.util.HashMap;
+
+/**
+ * @author: muyi
+ * @date: 2020-12-13 20:13
+ *
+ * 给定一个整数数组，判断是否存在重复元素。
+ *
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处.
+ *
+ *思路: hashmap
+ */
+public class leetcode_217_contains_duplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> hashMap = new HashMap<Integer, Integer>();
+        for(int i=0 ;i< nums.length;i++){
+            if (hashMap.containsKey(nums[i])) {
+                return true;
+            }
+            hashMap.put(nums[i],nums[i]);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicate(nums));
+    }
+}
